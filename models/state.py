d6d4d6cf206ce2adf508fc3edd09c80da10f57bf
modernfuthark <timothys1738@gmail.com>
2020-08-12 17:54:53 -0400

    pep8 indentations

--- models/state.py
+++ models/state.py
@@ -10,7 +10,7 @@ class State(BaseModel, Base):
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
     cities = relationship("Child", cascade="delete, delete-orphan",
-                           backref="state")
+                          backref="state")
 
     @property
     def cities(self):
