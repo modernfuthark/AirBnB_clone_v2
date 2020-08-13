0677aa5dc9969c9621b9662d0d77b4adaecd05cc
modernfuthark <timothys1738@gmail.com>
2020-08-13 18:36:05 -0400

    Added comments to file

--- models/city.py
+++ models/city.py
@@ -10,4 +10,5 @@ class City(BaseModel, Base):
     __tablename__ = "cities"
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
     name = Column(String(128), nullable=False)
+    # Establishes a relationship between Places and Cities
     places = relationship("Place", cascade="all, delete", backref="cities")
