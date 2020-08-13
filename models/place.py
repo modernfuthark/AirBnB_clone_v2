faddec3ec7365f4b556fc313217d1c5049ecc916
modernfuthark <timothys1738@gmail.com>
2020-08-13 18:37:32 -0400

    Added comments to file

--- models/place.py
+++ models/place.py
@@ -33,6 +33,7 @@ class Place(BaseModel, Base):
 
     # Establishes a relationship between Reviews and Places
     reviews = relationship("Review", cascade="all, delete", backref="place")
+    # Establishes a many-to-many relationship between Amenities and a table of place amenities
     amenities = relationship("Amenity", secondary=place_amenity,
                              viewonly=False)
 
