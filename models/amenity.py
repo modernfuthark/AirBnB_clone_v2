b1d3cf8a08aeee0c66da9cedb3288c4432f1116c
modernfuthark <timothys1738@gmail.com>
2020-08-13 18:36:02 -0400

    Added comments to file

--- models/amenity.py
+++ models/amenity.py
@@ -11,5 +11,6 @@ class Amenity(BaseModel, Base):
     __tablename__ = "amenities"
 
     name = Column(String(128), nullable=False)
+    # Establishes a relationship between amenities and places
     place_amenities = relationship("Place", secondary=place_amenity,
                                    viewonly=False)
