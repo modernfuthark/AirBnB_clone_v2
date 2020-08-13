5543eb3f8c72f7bbc1ebcb0f24b8e771d9da303f
95ktsmith <95ktsmith@gmail.com>
2020-08-13 01:19:52 +0000

    Description, latitude and longitude can be null and have no default value.

--- models/place.py
+++ models/place.py
@@ -16,6 +16,7 @@ place_amenity = Table("place_amenity", Base.metadata,
                       )
 """
 
+
 class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ = "places"
@@ -23,13 +24,13 @@ class Place(BaseModel, Base):
     city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
     user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     name = Column(String(128), nullable=False)
-    description = Column(String(1024), nullable=False, default=0)
+    description = Column(String(1024))
     number_rooms = Column(Integer, nullable=False, default=0)
     number_bathrooms = Column(Integer, nullable=False, default=0)
     max_guest = Column(Integer, nullable=False, default=0)
     price_by_night = Column(Integer, nullable=False, default=0)
-    latitude = Column(Float, nullable=False, default=0)
-    longitude = Column(Float, nullable=False, default=0)
+    latitude = Column(Float)
+    longitude = Column(Float)
     amenity_ids = []
 
     @property
