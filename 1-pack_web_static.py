171816f3f5589359fc8b1cebfa003987274fdea7
modernfuthark <timothys1738@gmail.com>
2020-08-17 22:09:44 -0400

    [N] Added file to directory

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,28 +1,17 @@
 #!/usr/bin/python3
-""" Pack Web Static """
+""" 1-pack_web_static """
+from os
 from datetime import datetime
 from fabric.api import local
-import os
 
 
 def do_pack():
-    """ Generates a .tgz archive from web_static directory """
-    # Generate filename
-    time = datetime.now().timetuple()
-    time_str = ""
-    for index in range(0, 6):
-        time_str = time_str + str(time[index])
-    filename = "versions/web_static_" + time_str + ".tgz"
+    """ does the packing """
+    dt = datetime.now().strftime("%Y%m%d%H%M%S")
+    file_name = "versions/web_static_{}.tgz".format(dt)
 
-    # Create destination directory
-    if not os.path.exists("versions"):
-        local("mkdir versions")
-
-    # Compress directory
-    local("tar -cvzf " + filename + " web_static")
-
-    # Test if successful
-    if os.path.exists(filename):
-        return filename
-    else:
-        return None
+    local("mkdir -p versions")
+    local("tar -czvf {} web_static".format(file_name))
+    if os.path.exists(file_name):
+        return file_name
+    return None
