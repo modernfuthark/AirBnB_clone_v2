dfd93e468499a1f9e821ce83d4347c41bfb08c01
modernfuthark <timothys1738@gmail.com>
2020-08-17 21:56:56 -0400

    [^] i am going to cry this is annoying

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -19,33 +19,6 @@ sudo ln -sf /data/web_static/releases/test /data/web_static/current
 # Change ownership of folder /data/
 sudo chown -R ubuntu:ubuntu /data/
 
-# i hate typing so use a shell variable for a string
-str='server {
-	listen 80 default_server;
-	listen [::]:80 default_server;
-	location /hbnb_static {
-	alias /data/web_static/current/;
-	index index.html;
-	}
-	location /tester {
-        alias /var/www/html/;
-        index tester.html;
-        }
-        rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;
-        add_header  X-Served-By $hostname;
-	error_page 404 /not_found.html;
-	location = /not_found.html {
-		root /usr/share/nginx/html;
-		internal;
-	}
-	root /var/www/html;
-	index index.html index.htm index.nginx-debian.html;
-	server_name _;
-	location / {
-		try_files $uri $uri/ =404;
-	}
-}'
-
 # Use string to edit default for nginx, then restart nginx
-sudo echo "$str" | sudo tee /etc/nginx/sites-available/default
+sudo sed -i '/pass the PHP/i\    location /hbnb_static {\n        alias /data/web_static/current;\n    }' /etc/nginx/sites-available/default
 sudo service nginx restart
