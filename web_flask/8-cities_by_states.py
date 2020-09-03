9cf3219d51d6708307e6f50274fa88e7d7542ae9
modernfuthark <timothys1738@gmail.com>
2020-09-02 21:30:52 -0400

    [N] task 9

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" 8-cities_by_states: Lists cities by states """
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+flap = Flask(__name__)
+
+
+@flap.route("/cities_by_states")
+def cities_by_states():
+    """ returns template html with cities listed by states """
+    states = storage.all(State)
+    return render_template("8-cities_by_states.html", states=states)
+
+@flap.teardown_appcontext
+def teardown(arg=None):
+    """ teardown """
+    storage.close()
+
+if __name__ == "__main__":
+    flap.url_map.strict_slashes = False
+    flap.run(host="0.0.0.0", port="5000")
